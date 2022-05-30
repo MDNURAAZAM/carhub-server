1139cc510d76734859ead3115bb97be007db8f17
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2022-05-30 06:49:10 +0200

    make admin api added

--- index.js
+++ index.js
@@ -90,6 +90,15 @@ async function run() {
       );
       res.send(result);
     });
+    app.put("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
   } finally {
   }
 }
