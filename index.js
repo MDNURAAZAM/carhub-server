c04037f3bbb25d02712eab157dcb38c2be6880bd
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2022-05-30 07:14:15 +0200

    delete user api added

--- index.js
+++ index.js
@@ -90,6 +90,19 @@ async function run() {
       );
       res.send(result);
     });
+
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+    app.delete("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await usersCollection.deleteOne(filter);
+      res.send(result);
+    });
     app.put("/user/admin/:email", async (req, res) => {
       const email = req.params.email;
       const filter = { email: email };
